f565d27d66b1c7f7627bd74748dd3d4940d9a7b3
OkabeRintarou <2573269786@qq.com>
2015-09-19 13:12:37 +0800

    tools

--- python/libs/misc/tools.py
+++ python/libs/misc/tools.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
+import sys,os
 import types	
 import inspect
+import linecache
+import signal,functools
 
 def info(obj,words_perline=4,width=20):
 	def print_info(string,lists):
@@ -9,25 +12,29 @@ def info(obj,words_perline=4,width=20):
 		space_width = len(string)
 		list_len = len(lists)
 		if list_len <= words_perline:
-			print(string,lists)
+			print(string,end='')
+			print(lists)
 		else:
 			spaces = ' ' * space_width	
-			print(string,lists[0:words_perline])
+			print(string,end='')
+			print(lists[0:words_perline])
 			i = words_perline
-			while (i << 1) < list_len:
-				print(spaces,lists[i:i << 1])
-				i <<= 1
-			print(spaces,[lists[i:]])
+			while (i + words_perline) < list_len:
+				print(spaces,end='')
+				print(lists[i:i + words_perline])
+				i += words_perline
+			print(spaces,end='')
+			print(lists[i:])
 	functions = get_function(obj)
 	modules = get_module(obj)
 	classes = get_class(obj)
 
 	if functions:
-		print('functions:')
+		print_info('functions:',functions)
 	if modules:
-		print('module:')
+		print_info('module:',modules)
 	if classes:
-		print('classes:')
+		print_info('classes:',classes)
 
 __target__ = {
 				'function':types.FunctionType,
@@ -47,3 +54,40 @@ def get_module(obj):
 
 def get_function(obj):
 	return __get_target__(obj,'function')
+
+class TimeoutError(Exception):pass
+
+def timeout(seconds,error_message = 'Function call timed out'):
+	def _timeout(func):
+		def _handle_timeout(signum,frame):
+			raise TimeoutError(error_message)
+		@functools.wraps(func)
+		def wrapper(*args,**kw):
+			signal.signal(signal.SIGALRM,_handle_timeout)
+			signal.alarm(seconds)
+			try:
+				result = func(*args,**kw)
+			finally:
+				signal.alarm(0)
+			return result
+		return wrapper
+	return _timeout
+
+def trace(f):
+	def globaltrace(frame,why,arg):
+		if why == 'call':return localtrace
+		return None
+	def localtrace(frame,why,arg):
+		if why == 'line':
+			filename = frame.f_code.co_filename
+			lineno = frame.f_lineno
+			bname = os.path.basename(filename)
+			print("{}({}):{}".format(bname,lineno,linecache.getline(filename,lineno)))
+		return localtrace
+	def _trace(*args,**kw):
+		sys.settrace(globaltrace)
+		result = f(*args,**kw)
+		sys.settrace(None)
+		return result
+	return _trace
+
